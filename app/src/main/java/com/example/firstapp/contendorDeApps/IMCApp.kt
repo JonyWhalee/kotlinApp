adb79786a0a0513c5467966c8dd5bda86b3df7ce
Jonathan Romero <jonathandr600@gmail.com>
2023-08-27 18:38:55 -0300

    weight and age components

--- app/src/main/java/com/example/firstapp/contendorDeApps/IMCApp.kt
+++ app/src/main/java/com/example/firstapp/contendorDeApps/IMCApp.kt
@@ -6,6 +6,7 @@ import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
 import com.example.firstapp.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.slider.RangeSlider
 import java.text.DecimalFormat
 
@@ -13,12 +14,23 @@ class IMCApp : AppCompatActivity() {
 
     private var isMaleSelected:Boolean = true
     private var isFemaleSelected:Boolean = false
+    private var currentWeight:Int = 0
+    private var currentAge:Int = 0
+
 
 
     private lateinit var  viewMale:CardView
     private lateinit var  viewFemale:CardView
     private lateinit var tvHeight:TextView
     private lateinit var rsHeight:RangeSlider
+    private lateinit var btnSubtractWeight:FloatingActionButton
+    private lateinit var btnPlusWeight:FloatingActionButton
+    private lateinit var tvWeight:TextView
+    private lateinit var btnSubtractAge:FloatingActionButton
+    private lateinit var btnPlusAge:FloatingActionButton
+    private lateinit var tvAge:TextView
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +45,12 @@ class IMCApp : AppCompatActivity() {
         viewFemale = findViewById(R.id.viewFemale)
         tvHeight = findViewById(R.id.tvHeight)
         rsHeight = findViewById(R.id.rsHeight)
+        btnSubtractWeight = findViewById(R.id.btnSubtractWeight)
+        btnPlusWeight = findViewById(R.id.btnPlusWeight)
+        tvWeight = findViewById(R.id.tvWeight)
+        btnSubtractAge = findViewById(R.id.btnSubtractAge)
+        btnPlusAge = findViewById(R.id.btnPlusAge)
+        tvAge = findViewById(R.id.tvAge)
     }
 
     private fun initListeners() {
@@ -50,6 +68,29 @@ class IMCApp : AppCompatActivity() {
 
             tvHeight.text = "$result cm"
         }
+        btnPlusWeight.setOnClickListener {
+            currentWeight++
+            setWeight()
+        }
+        btnSubtractWeight.setOnClickListener {
+            currentWeight--
+            setWeight()
+        }
+        btnPlusAge.setOnClickListener {
+            currentAge++
+            setAge()
+        }
+        btnSubtractAge.setOnClickListener {
+            currentAge--
+            setAge()
+        }
+    }
+    private fun setWeight(){
+        var result = "${currentWeight}kg"
+        tvWeight.text = result
+    }
+    private fun setAge(){
+        tvAge.text = currentAge.toString()
     }
 
     private fun changeGender(){
@@ -74,6 +115,8 @@ class IMCApp : AppCompatActivity() {
 
     private fun initUI() {
         setGenderColor()
+        setWeight()
+        setAge()
     }
 
 }
